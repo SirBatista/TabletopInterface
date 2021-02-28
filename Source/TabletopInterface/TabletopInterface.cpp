72b5389446ee5fba10753a835451afeaa9f9ba32
August Batista <august@horizonvp.com>
2021-02-27 20:48:11 -0500

    Added Flaws and Favors, adjusted social connections.

---
+++ Source/TabletopInterface/TabletopInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TabletopInterface.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TabletopInterface, "TabletopInterface" );
